869120c8e65264d02b05b7a7a2e6fe268026e83d
Евгений Картавых <evgeniy@kartavykh.com>
2024-11-02 10:27:41 +0300

    +

--- Sirius-WebApplication/Controllers/HomeController.cs
+++ Sirius-WebApplication/Controllers/HomeController.cs
@@ -21,8 +21,39 @@ namespace Sirius_WebApplication.Controllers
 
         public string Index()
         {
-            var s = String.Join(", ", _dbContext.Persons.Select(i => $"{i.FName} {i.LName}"));
+            var s = String.Join(", ", _dbContext.Persons.Select(i => $"{i.Id}. {i.FName} {i.LName}"));
             return String.IsNullOrEmpty(s) ? "еще никого нет" : s;
+        }
+
+
+        public string Add(Person person)
+        {
+            if (person != null && person.Valid())
+            {
+                _dbContext.Persons.Add(person);
+                _dbContext.SaveChanges();
+                return "Добавлено!";
+            }
+            else return "Не все атрибуты пользователя определены";
+        }
+
+        public string Edit(Person person)
+        {
+            if (person != null && person.Valid())
+            {
+                var p = _dbContext.Persons.FirstOrDefault(i => i.Id == person.Id);
+                if (p == null)
+                    return "Такого пользователя мы не нашли";
+                var updated = p.Update(person);
+                _dbContext.SaveChanges();
+                return "Были обновлены следующие атрибуты: " + String.Join(",", updated);
+            }
+            else return "Не все атрибуты пользователя определены";
+        }
+
+        public string Delete(Person person)
+        {
+            throw new NotImplementedException();
         }
 
         public IActionResult Privacy()
--- Sirius-WebApplication/Models/Person.cs
+++ Sirius-WebApplication/Models/Person.cs
@@ -1,4 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection;
 
 
 namespace Sirius_WebApplication.Models
@@ -9,5 +12,56 @@ namespace Sirius_WebApplication.Models
         public int Id { get; set; }
         public string FName { get; set; }
         public string LName { get; set; }
+
+
+
+        [NotMapped]
+        public virtual string[] NotNullProps => new string[] { "LName", "FName" };
+
+        /// <summary>
+        /// Проверка на наличие значений важных атрибутов
+        /// </summary>
+        /// <returns></returns>
+        public bool Valid()
+        {
+            bool valid = true;
+            foreach (var prop in NotNullProps)
+            {
+                var pi = this.GetType().GetProperty(prop);
+                if (pi != null)
+                {
+                    var value = pi.GetValue(this, null);
+                    valid &= !pi.PropertyType.IsDefaultValue(value) && value.ToString() != "";
+                }
+            }
+            return valid;
+        }
+
+        /// <summary>
+        /// Обновление значений атрибутов
+        /// </summary>
+        /// <param name="person"></param>
+        /// <returns></returns>
+        public IEnumerable<string> Update(Person person)
+        {
+            List<string> updated = new List<string>();
+            foreach (var pi in person.GetType().GetProperties().Where(i => i.CanWrite))
+            {
+                var v1 = pi.GetValue(this, null);
+                var v2 = pi.GetValue(person, null);
+                if (!v1.Equals(v2))
+                {
+                    pi.SetValue(this, v2, null);
+                    updated.Add(pi.Name);
+                }
+            }
+            return updated.ToArray();
+        }
+    }
+
+
+    public class Employeer : Person
+    {
+
     }
 }
